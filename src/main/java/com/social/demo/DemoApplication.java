ad01571dea34f2588dd21e9cc644485647132ae7
Lívia <116227611+lihbolivia@users.noreply.github.com>
2024-12-23 19:42:09 -0300

    demoApplication

--- src/main/java/com/social/demo/DemoApplication.java
+++ src/main/java/com/social/demo/DemoApplication.java
@@ -2,12 +2,15 @@ package com.social.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.social.demo.model.Conexao;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+	public static void main(String[] args) throws Exception {
+       
+        SpringApplication.run(DemoApplication.class, args);
+}
+	
 
 }
