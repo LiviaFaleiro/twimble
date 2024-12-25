be3ca96084280baf0639409f504ed1c60b1b7fb9
Lívia <116227611+lihbolivia@users.noreply.github.com>
2024-12-25 20:22:32 -0300

    create & getAll

--- src/main/java/com/social/demo/controller/PostsController.java
+++ src/main/java/com/social/demo/controller/PostsController.java
@@ -1,5 +1,38 @@
 package com.social.demo.controller;
 
+import com.social.demo.model.Posts;
+
+import java.util.ArrayList;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+
 public class PostsController {
     
+    private Posts posts;
+
+    @PostMapping("posts/criar")
+    public void criar(@RequestParam("texto") String texto, @RequestParam("id_usuario") int id_usuario){
+        posts = new Posts();
+        posts.setTexto(texto);
+        posts.setId_usuario(id_usuario);
+        posts.salvar();
 }
+    @GetMapping("posts/listar")
+    public ArrayList<Posts> listar(){
+        return Posts.getAll();
+    }
+    @GetMapping("posts/listar/{id}")
+    public Posts listar(@PathVariable("id") int id){
+        posts = new Posts();
+        posts.setId(id);
+        posts.load();
+        return this.posts;
+}
+}//fim da classe PostsController
--- src/main/java/com/social/demo/model/Posts.java
+++ src/main/java/com/social/demo/model/Posts.java
@@ -10,11 +10,10 @@ import java.util.List;
 public class Posts {
     private int id;
     private String texto;
-    private int curtidas;
+    private int n_curtidas;
     private int id_usuario;
-    private int comentarios;
+    private int n_comentarios;
 
-    
     public int getId() {
         return id;
     }
@@ -27,11 +26,11 @@ public class Posts {
     public void setTexto(String texto) {
         this.texto = texto;
     }
-    public int getCurtidas() {
-        return curtidas;
+    public int getN_curtidas() {
+        return n_curtidas;
     }
-    public void setCurtidas(int curtidas) {
-        this.curtidas = curtidas;
+    public void setN_curtidas(int n_curtidas) {
+        this.n_curtidas = n_curtidas;
     }
     public int getId_usuario() {
         return id_usuario;
@@ -39,11 +38,78 @@ public class Posts {
     public void setId_usuario(int id_usuario) {
         this.id_usuario = id_usuario;
     }
-    public int getComentarios() {
-        return comentarios;
+    public int getN_comentarios() {
+        return n_comentarios;
     }
-    public void setComentarios(int comentarios) {
-        this.comentarios = comentarios;
+    public void setN_comentarios(int n_comentarios) {
+        this.n_comentarios = n_comentarios;
     }
 
-}
+    public void salvar(){
+        Conexao c = new Conexao();
+        Connection dbConn = c.getConexao();
+
+        String sql = "INSERT INTO posts (texto, n_curtidas, id_usuario, n_comentarios) VALUES (?, ?, ?, ?)";
+        try {
+            PreparedStatement  ps = dbConn.prepareStatement(sql);
+            ps.setString(1, this.texto);
+            ps.setInt(2, this.n_curtidas);
+            ps.setInt(3, this.id_usuario);
+            ps.setInt(4, this.n_comentarios);
+            ps.executeUpdate();
+
+            
+            ResultSet rs =ps.getGeneratedKeys();
+            if(rs.next()) {
+                this.id = rs.getInt(1);
+            }
+        }
+        catch (SQLException e) {
+            System.out.println("Erro ao salvar post: " + e.getMessage());
+            e.printStackTrace();
+        }
+    } //FIM DO SALVAR
+    public static ArrayList<Posts> getAll() {
+        ArrayList<Posts> lista = new ArrayList<Posts>();
+        Conexao c = new Conexao();
+        Connection dbConn = c.getConexao();
+        String sql = "SELECT * FROM posts";
+        try {
+            Statement stmt = dbConn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()) {
+                Posts post = new Posts();
+                post.setId(rs.getInt("id"));
+                post.setTexto(rs.getString("texto"));
+                post.setN_curtidas(rs.getInt("n_curtidas"));
+                post.setId_usuario(rs.getInt("id_usuario"));
+                post.setN_comentarios(rs.getInt("n_comentarios"));
+                lista.add(post);
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro ao listar posts: " + e.getMessage());
+            e.printStackTrace();    
+        } return lista;
+    }
+
+    public void load() {
+        Conexao c = new Conexao();
+        Connection dbConn = c.getConexao();
+        String sql = "SELECT * FROM posts WHERE id = ?";
+        try {
+            PreparedStatement ps = dbConn.prepareStatement(sql);
+            ps.setInt(1, this.id);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()) {
+                this.texto = rs.getString("texto");
+                this.n_curtidas = rs.getInt("n_curtidas");
+                this.id_usuario = rs.getInt("id_usuario");
+                this.n_comentarios = rs.getInt("n_comentarios");
+                }          
+            }catch (SQLException e) {
+                System.out.println("Erro ao carregar post: " + e.getMessage());
+                e.printStackTrace();
+            }
+    }
+    
+} //fim da classe
